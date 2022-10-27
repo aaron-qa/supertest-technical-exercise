b7c1209395a761014c5ea1807c4602f86b3ead58
Aaron Williams <aaron.williams@acin.com>
2022-10-27 17:02:43 +0100

    Creating repo

--- src/test/test.spec.js
+++ src/test/test.spec.js
@@ -1,9 +1,8 @@
 const request = require('supertest');
 
-describe('Acin.com', function() {    
+describe('Acin.com', () => {    
     it('should repond with 200 OK', async () => {
         const response = await request('https://www.acin.com').get('/')
-        
         expect(response.status).toEqual(200);
       });
   });
